f51113bc61744a67b7e83a47b264a1d9b8a53517
nayeon Kim <kimnayeon574@gmail.com>
2019-05-21 15:34:31 +0900

    #2 타이머 컨트롤과 텍스트 박스 컨트롤을 사용하여 카운트 기능 구현

--- Timer/Form1.Designer.cs
+++ Timer/Form1.Designer.cs
@@ -51,10 +51,12 @@
             this.btnCount.TabIndex = 1;
             this.btnCount.Text = "카운트다운";
             this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
             // 
             // Timer
             // 
             this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
             // txtCountDown
             // 
--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -7,13 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0;
+
+        private bool intCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("숫자만 입력해주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            if (intCheck())
+            {
+                CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.Timer.Enabled = true;
+                this.txtNum.ReadOnly = true;
+            }
+            else
+            {
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false; 
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtCountDown.Text = "";
+                MessageBox.Show("펑!", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                this.txtNum.Focus();
+            }
+            else
+            {
+                CountOrgNum--;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+            }
+
         }
     }
 }
